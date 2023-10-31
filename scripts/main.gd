250b7f55b2eb76798ce63abf310ba82dabad4a54
SeanCampbell255 <campbellsean211@gmail.com>
2023-10-31 17:53:44 -0400

    HUD
    HUD shows start game button, game title, player lives & score

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,13 +2,42 @@ extends Node
 
 @export var rock_scene : PackedScene
 
+var level = 0
+var playing = false
+var score = 0
 var screensize = Vector2.ZERO
+
+func _process(delta):
+	if not playing:
+		return
+	if get_tree().get_nodes_in_group("rocks").size() == 0:
+		new_level()
 
 func _ready():
 	screensize = get_viewport().get_visible_rect().size
-	for i in 3:
+
+func game_over():
+	playing = false
+	$HUD.game_over() 
+
+func new_game():
+	get_tree().call_group("rocks", "queue_free")
+	
+	level = 0
+	score = 0
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready!")
+	
+	$Player.reset()
+	await $HUD/Timer.timeout
+	playing = true
+
+func new_level():
+	level += 1
+	$HUD.show_message("Wave %s" % level)
+	for i in level:
 		spawn_rock(3)
-		
+
 func spawn_rock(size, pos=null, vel=null):
 	if pos == null:
 		$RockPath/RockSpawn.progress = randi()
