6a12172b71d8f775da94eba7977a47aa94429755
SeanCampbell255 <campbellsean211@gmail.com>
2023-11-03 13:33:19 -0400

    Shield fully functional, shows in UI
    Player also takes damage from enemy body + bullets

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -5,7 +5,19 @@ signal start_game
 @onready var lives_counter = $MarginContainer/HBoxContainer/LivesCounter.get_children()
 @onready var score_label = $MarginContainer/HBoxContainer/ScoreLabel
 @onready var message = $VBoxContainer/Message
+@onready var shield_bar = $MarginContainer/HBoxContainer/ShieldBar
 @onready var start_button = $VBoxContainer/StartButton
+
+var bar_textures = {
+	"green": preload("res://assets/bar_green_200.png"),
+	"yellow": preload("res://assets/bar_yellow_200.png"),
+	"red": preload("res://assets/bar_red_200.png")
+}
+
+func game_over():
+	show_message("Game Over")
+	await $Timer.timeout
+	start_button.show()
 
 func show_message(text):
 	message.text = text
@@ -19,10 +31,13 @@ func update_lives(value):
 	for item in 3:
 		lives_counter[item].visible = value > item
 
-func game_over():
-	show_message("Game Over")
-	await $Timer.timeout
-	start_button.show()
+func update_shield(value):
+	shield_bar.texture_progress = bar_textures["green"]
+	if value < 0.4:
+		shield_bar.texture_progress = bar_textures["red"]
+	elif value < 0.7:
+		shield_bar.texture_progress = bar_textures["yellow"]
+	shield_bar.value = value
 
 func _on_start_button_pressed():
 	start_button.hide()
